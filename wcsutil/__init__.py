03a2c5643a4c3c10ed7cb05a44facd150bdfc7a5
dencheva <dencheva@stsci.edu>
2009-02-10 21:07:05 +0000

    Cleaned code and added comments
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/development/trunk/updatewcs@7535 fe389314-cf27-0410-b35b-8c050e845b92

--- wcsutil/__init__.py
+++ wcsutil/__init__.py
@@ -175,11 +175,7 @@ class HSTWCS(WCS):
         cdmat = N.array([[cd11, cd12],[cd21,cd22]])
         self.wcs.cd = cdmat * self.pscale/3600
         
-    def verifyPa_V3(self):
-        """make sure PA_V3 is populated"""    
-        
-    def verifyKw(self):
-        """verify that all required kw have meaningful values"""
+
             
     def readModel(self, update=False, header=None):
         """
@@ -257,18 +253,25 @@ class HSTWCS(WCS):
         ext_hdr.update('IDCV3REF', self.idcmodel.refpix['V3REF'])
         
     
-    def help(self):
-        print 'How to create an HSTWCS object:\n\n'
-        print """ \
-        1. Using a pyfits HDUList object and an extension number \n
-        Example:\n
-        
-        fobj = pyfits.open('some_file.fits')\n
-        w = wcsutil.HSTWCS(fobj, 3)\n\n
-        
-        2. Create an HSTWCS object using a qualified file name. \n
-        Example:\n
-        w = wcsutil.HSTWCS('j9irw4b1q_flt.fits[sci,1]')\n\n
-        """
-        
+def help():
+    print 'How to create an HSTWCS object:\n\n'
+    print """ \
+    1. Using a pyfits HDUList object and an extension number \n
+    Example:\n
+    
+    fobj = pyfits.open('some_file.fits')\n
+    w = wcsutil.HSTWCS(fobj, 3)\n\n
+    
+    2. Create an HSTWCS object using a qualified file name. \n
+    Example:\n
+    w = wcsutil.HSTWCS('j9irw4b1q_flt.fits[sci,1]')\n\n
+    
+    3. Create an HSTWCS object using a file name and an extension number. \n
+    Example:\n
+    w = wcsutil.HSTWCS('j9irw4b1q_flt.fits', ext=2)\n\n
+    
+    4. Create a template HSTWCS object for an instrument/detector combination.]n
+    Example:\n
+    w = wcsutil.HSTWCS(instrument='ACS', detector='WFC'\n\n
+    """
     
